6ed291598dbf255e43a8037161a531daed8ffbf4
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2025-03-15 14:03:46 -0300

    forçando a esteira

--- lambda-payment-function/src/index.js
+++ lambda-payment-function/src/index.js
@@ -1,6 +1,6 @@
 exports.lambda_handler = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify("Hello from Camilo CICD!"),
+    body: JSON.stringify("Teste de esteira!"),
   };
 };
