1735f3456e20948e84fbcef9bb0af5eaa048c555
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2025-03-15 14:27:56 -0300

    ajuste no bucket name

--- lambda-payment-function/esbuild.config.js
+++ lambda-payment-function/esbuild.config.js
@@ -1,14 +1,37 @@
 const esbuild = require('esbuild');
+const fs = require('fs');
+const archiver = require('archiver');
 
+const outputDir = './dist';
+const outputFile = `${outputDir}/index.js`;
+const zipFile = `${outputDir}/lambda-payment-function.zip`;
+
+// Build the bundled file
 esbuild.build({
   entryPoints: ['./src/index.js'], // Caminho para o arquivo principal
   bundle: true,                   // Agrupa todas as dependências
   minify: true,                   // Minifica o código
   platform: 'node',               // Define o ambiente como Node.js
   target: 'node18',               // Define a versão do Node.js
-  outfile: './dist/index.js',     // Arquivo de saída
+  outfile: outputFile,            // Arquivo de saída
 }).then(() => {
   console.log('Build completed successfully.');
+
+  // Create a ZIP file
+  const output = fs.createWriteStream(zipFile);
+  const archive = archiver('zip', { zlib: { level: 9 } });
+
+  output.on('close', () => {
+    console.log(`ZIP file created: ${zipFile} (${archive.pointer()} total bytes)`);
+  });
+
+  archive.on('error', (err) => {
+    throw err;
+  });
+
+  archive.pipe(output);
+  archive.file(outputFile, { name: 'index.js' });
+  archive.finalize();
 }).catch((error) => {
   console.error('Build failed:', error);
   process.exit(1);
