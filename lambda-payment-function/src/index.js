b930579521e20b4ac3b38be05aacb104fb3bd916
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2025-03-16 10:53:22 -0300

    ajsute para dar a permissão do lmabda publicar o evento

--- lambda-payment-function/src/index.js
+++ lambda-payment-function/src/index.js
@@ -1,9 +1,31 @@
+import { EventBridgeClient, PutEventsCommand } from "@aws-sdk/client-eventbridge";
 import log from "lambda-log"; // Use ES module import
+const eventBridgeClient = new EventBridgeClient();
 
-export const lambda_handler = async (event) => {
-  log.info(JSON.stringify(event));
-  return {
-    statusCode: 200,
-    body: "Camilo Coelho Gomes",
-  };
+
+export const lambda_handler = async (event, context) => {
+  try {
+    log.info(JSON.stringify(event));
+    await eventBridgeClient.send(new PutEventsCommand({
+      Entries: [
+        {
+          EventBusName: 'tcc-event-bus',
+          Source: context.function_name,
+          DetailType: 'payment',
+          Detail: JSON.stringify({
+            amount: 100,
+            paymentType: 'credit_card',
+          }),
+        },
+      ],
+    }));
+  } catch (error) {
+    log.error(error);
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
+
+  }
+
 };
