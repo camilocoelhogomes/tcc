a18c7eca0c07f755f70ccc4c365916560f658ffa
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2025-03-17 08:43:38 -0300

    ajustando a publicação do event

--- lambda-payment-function/src/index.js
+++ lambda-payment-function/src/index.js
@@ -5,43 +5,18 @@ const eventBridgeClient = new EventBridgeClient();
 export const lambda_handler = async (event, context) => {
   const logger = new LambdaLog();
   logger.info({ event, context });
-  return;
 
-  const correlationId = event.requestContext.extendedRequestId;
-  logger.options.meta.correlationId = correlationId
-  logger.options.meta.functionName = context.functionName;
-  logger.options.meta.requestId = context.awsRequestId;
 
   try {
     const body = JSON.parse(event.body);
     const putEventsComand = {
-      Entries: [
-        {
-          EventBusName: 'tcc-event-bus',
-          Source: context.functionName,
-          DetailType: 'payment',
-          Detail: JSON.stringify({
-            header: {
-              correlationId
-            },
-            body
-          }),
-        },
-      ],
+      Entries: event.Records.map(r => recordHandler(r, context)),
     };
     const result = await eventBridgeClient.send(new PutEventsCommand(putEventsComand));
     if (result.FailedEntryCount > 0) {
       logger.error(`Erro ao publicar evento ${JSON.stringify(result)}`);
       throw result.Entries.filter(entry => entry.ErrorCode);
     }
-    logger.info(`TCC - Log FunctionName: ${context.functionName} CorrelationId: ${correlationId}`);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Evento publicado com sucesso",
-        result,
-      }),
-    };
   } catch (error) {
     logger.error(JSON.stringify(error));
     return {
@@ -53,3 +28,23 @@ export const lambda_handler = async (event, context) => {
     };
   }
 };
+
+export const recordHandler = async (event, context) => {
+  const logger = new LambdaLog();
+  const correlationId = event.messageAttributes.correlationId;
+  logger.options.meta.correlationId = correlationId
+  logger.options.meta.functionName = context.functionName;
+  logger.options.meta.requestId = context.awsRequestId;
+  logger.info(`TCC - Log FunctionName: ${context.functionName} EventSource: ${event.source} CorrelationId: ${event.detail.header.correlationId}`);
+  return {
+    EventBusName: 'tcc-event-bus',
+    Source: context.functionName,
+    DetailType: 'payment',
+    Detail: JSON.stringify({
+      header: {
+        correlationId
+      },
+      body: JSON.parse(event.body)
+    }),
+  }
+}
