a60a9fae7d748031ee0cbf883c6b272edb31d374
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2025-03-17 08:20:20 -0300

    publish deploy

--- lambda-payment-function/src/index.js
+++ lambda-payment-function/src/index.js
@@ -4,7 +4,6 @@ const eventBridgeClient = new EventBridgeClient();
 
 export const lambda_handler = async (event, context) => {
   const logger = new LambdaLog();
-
   logger.info({ event, context });
   return;
   const correlationId = event.requestContext.extendedRequestId;
