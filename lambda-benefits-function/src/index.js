84e6f2bd1b480226ecce3057c3c0e133793dc5be
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2025-04-01 06:31:39 -0300

    processo de idepotencia

--- lambda-benefits-function/src/index.js
+++ lambda-benefits-function/src/index.js
@@ -1,11 +1,10 @@
-import { LambdaLog } from "lambda-log"; // Use ES module import
-
+import { LambdaLog } from "lambda-log";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+const client = new DynamoDBClient({});
 
 export const lambda_handler = async (event, context) => {
 
-  for await (const record of event.Records) {
-    await messageHandler(JSON.parse(record.body), context);
-  }
+  await Promise.all(event.Records.map(r => messageHandler(JSON.parse(r.body), context)));
 
 };
 
@@ -16,10 +15,27 @@ export const messageHandler = async (event, context) => {
   logger.options.meta.functionName = context.functionName;
   logger.options.meta.requestId = context.awsRequestId;
   try {
+    await idepotencyCheck(event.detail.header.correlationId, context.functionName, logger);
     logger.info(`TCC - Log FunctionName: ${context.functionName} EventSource: ${event.source} CorrelationId: ${event.detail.header.correlationId}`);
   } catch (error) {
-    logger.error(error);
-    throw error;
+    logger.error(`TCC - idepotency check for correlationId ${event.detail.header.correlationId} failed: ${error}`);
+    return;
   }
 
 }
+
+export const idepotencyCheck = async (eventId, lambdaName, logger) => {
+  logger.info(`TCC - Log IdepotencyCheck: ${lambdaName} EventId: ${eventId}`);
+  const ttl = Math.floor(Date.now() / 1000) + 24 * 60 * 60;
+  const params = {
+    TableName: "IdepotencyTable",
+    Item: {
+      pk: eventId,
+      sk: lambdaName,
+      ttl: ttl,
+    },
+    ConditionExpression: "attribute_not_exists(pk) AND attribute_not_exists(sk)", // Ensures idempotency
+  };
+
+  return await client.send(new PutCommand(params));
+};
