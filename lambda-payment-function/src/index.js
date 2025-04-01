d0451be102c578e3060442982990e9d125a54f73
Camilo Coelho Gomes <camilo.coelho.gomes@gmail.com>
2025-04-01 06:31:11 -0300

    teste de idepotencia

--- lambda-payment-function/src/index.js
+++ lambda-payment-function/src/index.js
@@ -8,13 +8,12 @@ export const lambda_handler = async (event, context) => {
 
 
   try {
-    const entries = event.Records.map(r => recordHandler(r, context));
+    const entries = await Promise.all(event.Records.map(r => recordHandler(r, context)));
     const putEventsComand = {
-      Entries: entries,
+      Entries: entries.filter(entry => entry),
     };
 
     const result = await eventBridgeClient.send(new PutEventsCommand(putEventsComand));
-    await new Promise((resolve) => setTimeout(resolve, 100));
     if (result.FailedEntryCount > 0) {
       logger.error(`Erro ao publicar evento ${JSON.stringify(result)}`);
       throw result.Entries.filter(entry => entry.ErrorCode);
@@ -25,7 +24,7 @@ export const lambda_handler = async (event, context) => {
   }
 };
 
-export const recordHandler = (event, context) => {
+export const recordHandler = async (event, context) => {
   const logger = new LambdaLog();
 
   const correlationId = event.messageAttributes.correlationId.stringValue;
@@ -33,6 +32,12 @@ export const recordHandler = (event, context) => {
   logger.options.meta.functionName = context.functionName;
   logger.options.meta.requestId = context.awsRequestId;
   logger.info(`TCC - Log FunctionName: ${context.functionName} CorrelationId: ${correlationId}`);
+  try {
+    await idepotencyCheck(correlationId, context.functionName, logger);
+  } catch (error) {
+    logger.error(`TCC - idepotency check for correlationId ${correlationId} failed: ${error}`);
+    return;
+  }
   const eventMapped = {
     EventBusName: 'tcc-event-bus',
     Source: context.functionName,
@@ -46,3 +51,19 @@ export const recordHandler = (event, context) => {
   }
   return eventMapped;
 }
+
+export const idepotencyCheck = async (eventId, lambdaName, logger) => {
+  logger.info(`TCC - Log IdepotencyCheck: ${lambdaName} EventId: ${eventId}`);
+  const ttl = Math.floor(Date.now() / 1000) + 24 * 60 * 60;
+  const params = {
+    TableName: "IdepotencyTable",
+    Item: {
+      pk: eventId,
+      sk: lambdaName,
+      ttl: ttl,
+    },
+    ConditionExpression: "attribute_not_exists(pk) AND attribute_not_exists(sk)", // Ensures idempotency
+  };
+
+  return await client.send(new PutCommand(params));
+};
